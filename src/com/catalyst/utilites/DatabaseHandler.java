8d703f2236971c8e88e799f64f2c726204f07b5e
unknown <ajackson@PC30123.catalystsolves.com>
2014-02-04 11:43:34 -0800

    minor changes and additions

---
+++ src/com/catalyst/utilites/DatabaseHandler.java
@@ -0,0 +1,33 @@
+package com.catalyst.utilites;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+	//constants
+	private static final int DB_VERSION = 1;
+	private static final String DB_NAME = "TBD";
+	
+
+	public DatabaseHandler(Context context) {
+		super(context, DB_NAME, null, DB_VERSION);
+		// TODO Auto-generated constructor stub
+		
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
